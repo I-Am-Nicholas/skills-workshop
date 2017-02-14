d45de730890fedf0aed3c9968f1c083c15429cea
Nicholas Leacock <nicholas@swissmail.com>
2017-02-14 21:14:48 +0000

    Rectangle solved + Specced

--- week-1/rectangle_kata/lib/rectangle.rb
+++ week-1/rectangle_kata/lib/rectangle.rb
@@ -1,3 +1,11 @@
+def header_footer
+ "|" + ('-' * 18) + "|" + "\n"
+end
+
+def body
+ ("|" + (' ' * 18) + "|" + "\n") * 8
+end
+
 def rectangle
-  "THIS SHOULD BE A RECTANGLE!"
+ header_footer + body + header_footer
 end
--- week-1/rectangle_kata/spec/rectangle_spec.rb
+++ week-1/rectangle_kata/spec/rectangle_spec.rb
@@ -1,16 +1,35 @@
 require "rectangle"
 
-describe "returning a 20x10 rectangle" do
-  it "returns a rectangle of the right size" do
-    expect(rectangle).to eq("|------------------|\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|                  |\n" +
-                            "|------------------|\n")
+describe "returning a line of 20 dashes betweeen 2 bars" do
+ it "returns a line of the right size" do
+   expect(header_footer).to eq("|------------------|\n")
+ end
+end
+
+describe "returning 8 lines fo 20 spaces between 2 bars" do
+  it "returns a line of the right size" do
+    expect(body).to eq("|                  |\n" +
+                      "|                  |\n" +
+                      "|                  |\n" +
+                      "|                  |\n" +
+                      "|                  |\n" +
+                      "|                  |\n" +
+                      "|                  |\n" +
+                      "|                  |\n")
+    end
   end
+
+describe "returning a 20x10 rectangle" do
+ it "returns a rectangle of the right size" do
+   expect(rectangle).to eq("|------------------|\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|                  |\n" +
+                           "|------------------|\n")
+ end
 end
